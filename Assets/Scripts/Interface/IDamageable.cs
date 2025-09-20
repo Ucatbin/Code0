f1f0db7ade2c4943575577d1db8a928ce7f32c8d
Ucatbin <1413709774@qq.com>
2025-09-20 14:21:41 +0800

    伤害接口

---
+++ Assets/Scripts/Interface/IDamageable.cs
@@ -0,0 +1,11 @@
+/// <summary>
+/// 可受伤接口，所有可以被攻击的物体都应该实现此接口
+/// </summary>
+public interface IDamageable
+{
+    int CurrentHealth { get; }
+    int MaxHealth { get; }
+    void TakeDamage(DamageData damageData);
+    void Die();
+}
+
