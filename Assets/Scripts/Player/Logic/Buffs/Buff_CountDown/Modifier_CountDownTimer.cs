017ab15b01a7c60ac182d520c8612b97b5afcb47
Ucatbin <1413709774@qq.com>
2025-10-21 01:19:39 +0800

    小修补

--- Assets/Scripts/Player/Logic/Buffs/Buff_CountDown/Modifier_CountDownTimer.cs
+++ Assets/Scripts/Player/Logic/Buffs/Buff_CountDown/Modifier_CountDownTimer.cs
@@ -9,6 +9,9 @@ public class Modifier_CountDownTimer : BaseBuffModifier
     {
         var buff = buffInfo.ConvertTo<BuffItem_CountDown>();
         buff.CountDownTimer -= 0.1f;
+        BuffEvents.TriggerBuffUpdate(buff.CountDownTimer, buffInfo);
+
+        // TODO: Use event to update display
         var display = Mathf.Max(buff.CountDownTimer, 0);
         buff.CountDownDisplay.text = display.ToString("F1");
     }
--- Assets/Scripts/Player/Logic/Buffs/PlayerBuffHandler.cs
+++ Assets/Scripts/Player/Logic/Buffs/PlayerBuffHandler.cs
@@ -3,12 +3,12 @@ using UnityEngine;
 
 public class PlayerBuffHandler : BuffHandler
 {
-    [SerializeField] BuffDataSO_CountDown PBuff_CountDown;
+    [SerializeField] BuffDataSO_CountDown Buff_CountDown;
     [SerializeField] TextMeshProUGUI _countDownDisplay;
 
     void Start()
     {
-        var P_CountDown = new BuffItem_CountDown(PBuff_CountDown, _entity, _entity, 1, _countDownDisplay);
+        var P_CountDown = new BuffItem_CountDown(Buff_CountDown, _entity, _entity, 1, _countDownDisplay);
         _entity.BuffSys.AddBuff(P_CountDown);
     }
 }
