05fd4d6d828080a25c25b574a57c688c2e4c35a6
Ucatbin <1413709774@qq.com>
2025-11-04 00:08:36 +0800

    1

--- Assets/Scripts/Test/EntitySystem/Core/EntityData.cs
+++ Assets/Scripts/Test/EntitySystem/Core/EntityData.cs
@@ -5,7 +5,14 @@ namespace ThisGame.EntitySystem
     [CreateAssetMenu(fileName = "EntityData", menuName = "Game/EntitySystem/New EntityData")]
     public class EntityData : ScriptableObject
     {
+        public string EntityName;
+        [HideInInspector] public int EntityHash;
         public float MaxHelth;
         public float BaseSpeed;
+
+        void OnValidate()
+        {
+            EntityHash = EntityName.GetHashCode();
+        }
     }
 }
